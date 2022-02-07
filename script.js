73e06bd8fb94020380ced3bc507a9a50f9a03f19
nikhilstacks <nikhilstacks@gmail.com>
2022-02-07 12:16:48 +0530

    final commit

--- script.js
+++ script.js
@@ -1,11 +1,53 @@
-const mainDiv = document.createElement('div');
-mainDiv.style.cssText = "padding: 350px; background-color: white;";
+const grid = document.querySelector('.grid');
+let gridValue = document.querySelector('.grid-size');
+let gridSize = document.querySelector('input');
+const resetBtn = document.querySelector('.reset');
+const applyGridSize = document.querySelector('.apply');
+let squareSize = 8;
 
-const body = document.body;
-document.body.style.cssText = 'background-color: #393E46;display: flex;justify-content: center;padding: 100px;';
+createGrid(squareSize);
 
-body.appendChild(mainDiv);
+// Create Squared Divs
+function createDiv(size) {
+  const div = document.createElement('div');
+  div.classList.add('box');
+  div.style.width = `${size}px`;
+  div.style.height = `${size}px`;
 
+  return div;
+}
 
+// Creat The Grid and append it to grid
+function createGrid(gridSize) {
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      grid.appendChild(createDiv(grid.clientWidth / gridSize));
+    }
+  }
+}
 
+function reset() {
+  while (grid.firstChild) {
+    grid.removeChild(grid.lastChild);
+  }
+  createGrid(squareSize);
+}
 
+// Used event delegation to target children of the grid
+grid.addEventListener('mouseover', function (e) {
+  // Add the "active" class to only divs with a "box" class
+  if (e.target.matches('.box')) {
+    e.target.classList.add('active');
+  }
+});
+
+gridSize.addEventListener('input', function (e) {
+  squareSize = e.target.value;
+  gridValue.textContent = `${squareSize}x${squareSize}`;
+});
+
+applyGridSize.addEventListener('click', function () {
+  reset();
+});
+
+resetBtn.addEventListener('click', reset);
