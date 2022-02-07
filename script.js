2514342f20a14c145a91c49cd836e9816deaadb8
nikhilstacks <nikhilstacks@gmail.com>
2022-02-07 11:07:41 +0530

    added first div

---
+++ script.js
@@ -0,0 +1,11 @@
+const mainDiv = document.createElement('div');
+mainDiv.style.cssText = "padding: 350px; background-color: white;";
+
+const body = document.body;
+document.body.style.cssText = 'background-color: #393E46;display: flex;justify-content: center;padding: 100px;';
+
+body.appendChild(mainDiv);
+
+
+
+
